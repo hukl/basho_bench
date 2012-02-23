090e3409fcb007f1ce4aa74d4fd5c7326db900ce
root <root@loft8133.serverloft.de>
2012-02-23 18:31:39 +0100

    add custom benchmarks

--- src/basho_bench_driver_riakc_pb.erl
+++ src/basho_bench_driver_riakc_pb.erl
@@ -89,6 +89,7 @@ new(Id) ->
 
 run(get, KeyGen, _ValueGen, State) ->
     Key = KeyGen(),
+    % error_logger:info_msg("Key: ~p~n", [Key]),
     case riakc_pb_socket:get(State#state.pid, State#state.bucket, Key,
                              [{r, State#state.r}]) of
         {ok, _} ->
@@ -98,6 +99,21 @@ run(get, KeyGen, _ValueGen, State) ->
         {error, Reason} ->
             {error, Reason, State}
     end;
+
+run(get_gamestate, KeyGen, _ValueGen, State) ->
+    Key = list_to_binary(
+         integer_to_list( 100000000000000 + random:uniform(50000000) ) 
+    ),
+    case riakc_pb_socket:get(State#state.pid, <<"gamestate">>, Key,
+                             [{r, State#state.r}]) of
+        {ok, _} ->
+            {ok, State};
+        {error, notfound} ->
+            {ok, State};
+        {error, Reason} ->
+            {error, Reason, State}
+    end;
+
 run(get_existing, KeyGen, _ValueGen, State) ->
     Key = KeyGen(),
     case riakc_pb_socket:get(State#state.pid, State#state.bucket, Key,
@@ -112,6 +128,18 @@ run(get_existing, KeyGen, _ValueGen, State) ->
 run(put, KeyGen, ValueGen, State) ->
     Robj0 = riakc_obj:new(State#state.bucket, KeyGen()),
     Robj = riakc_obj:update_value(Robj0, ValueGen()),
+    case riakc_pb_socket:put(State#state.pid, Robj, [{w, State#state.w},
+                                                     {dw, State#state.dw}]) of
+        ok ->
+            {ok, State};
+        {error, Reason} ->
+            {error, Reason, State}
+    end;
+run(put_gamestate, _, _, State) ->
+    Key = random_gamestate_key(100000000),
+    % error_logger:info_msg("Key: ~p~n", [Key]),
+    Robj0 = riakc_obj:new(<<"gamestate">>, Key),
+    Robj = riakc_obj:update_value(Robj0, generate_gamestate()),
     case riakc_pb_socket:put(State#state.pid, Robj, [{w, State#state.w},
                                                      {dw, State#state.dw}]) of
         ok ->
@@ -178,6 +206,13 @@ run(listkeys, _KeyGen, _ValueGen, State) ->
         {error, Reason} ->
             {error, Reason, State}
     end;
+
+run(m_values, KeyGen, _ValueGen, State) ->
+    case mapred_values( KeyGen, State ) of
+      {ok, _Result}     -> {ok, State};
+      {error, Reason}   -> {error, Reason, State}
+    end;
+
 run(mr_bucket_erlang, _KeyGen, _ValueGen, State) ->
     mapred(State, State#state.bucket, ?ERLANG_MR);
 run(mr_bucket_js, _KeyGen, _ValueGen, State) ->
@@ -190,6 +225,34 @@ run(mr_keylist_js, KeyGen, _ValueGen, State) ->
     Keylist = make_keylist(State#state.bucket, KeyGen,
                           State#state.keylist_length),
     mapred(State, Keylist, ?JS_MR).
+
+%% ====================================================================
+%% Key Generators
+%% ====================================================================
+
+random_highscore_key(MaxKey) ->
+    list_to_binary([
+        integer_to_list(100000000 + random:uniform(MaxKey)),
+        "_2012-02-01"
+      ]).
+
+random_gamestate_key(MaxKey) ->
+    list_to_binary(
+        integer_to_list( 200000000000000 + random:uniform(MaxKey) )
+    ).
+
+
+%% ====================================================================
+%% Value Generators
+%% ====================================================================
+
+generate_gamestate() ->
+    list_to_binary(
+        "{\"low_awards\":{\"award1\":5,\"award2\":3,\"award3\":2,\"award4\":4,\"award5\":0,\"award6\":2,\"award7\":1,\"award8\":0,\"award9\":0,\"award10\":0,\"crown1\":2,\"crown2\":0,\"crown3\":1},\"user_awards\":{\"type1_trophy1\":0,\"type1_trophy2\":0,\"type1_trophy3\":0,\"type1_trophy4\":0,\"type1_trophy5\":0,\"type1_trophy6\":0,\"type1_trophy7\":0,\"type1_trophy8\":0,\"type1_trophy9\":0,\"type1_trophy10\":0,\"type2_trophy1\":0,\"type2_trophy2\":0,\"type2_trophy3\":0,\"type2_trophy4\":0,\"type2_trophy5\":0,\"type2_trophy6\":0,\"type2_trophy7\":0,\"type2_trophy8\":0,\"type2_trophy9\":0,\"type2_trophy10\":0,\"type3_trophy1\":0,\"type3_trophy2\":0,\"type3_trophy3\":0,\"type3_trophy4\":0,\"type3_trophy5\":0,\"type3_trophy6\":0,\"type3_trophy7\":0,\"type3_trophy8\":0,\"type3_trophy9\":0,\"type3_trophy10\":0},\"low_scores\":{\"low_count\":17,\"low_count_week\":5,\"score\":114775,\"created_at\":\"2011-09-22 14:33:18\",\"calendar_week\":\"2011-09-19\",\"low_prem\":0,\"low_time_boosts\":4,\"low_add_timer_boosts\":3,\"low_score_boosts\":4,\"low_fireball_boosts\":0},\"user_days\":{\"days_in_a_row\":2,\"updated_at\":\"2011-09-22 14:33:18\"},\"user_missions\":{\"type\":2,\"mission\":1,\"tasks\":[{\"id\":1,\"it\":1,\"cr\":10,\"tr\":10}],\"state\":2,\"created_at\":\"2011-09-22 14:33:18\",\"updated_at\":\"2011-09-22 14:33:18\"},\"user\":{\"number_of_friends_playing\":8,\"number_of_friends\":240,\"locale\":\"en\",\"session_token\":\"2.1KlOvR_q32GbNbIijTSrHQ__.3600.1295\",\"created_at\":\"2011-09-22 14:33:18\",\"updated_at\":\"2011-09-22 14:33:18\",\"ip_address\":1121752565,\"rounds_played\":322},\"user_user_currency\":{\"gold\":20,\"coins\":500}}"
+    ).
+
+generate_highscore() ->
+    list_to_binary( integer_to_list( random:uniform(1000000) ) ).
 
 %% ====================================================================
 %% Internal functions
@@ -222,3 +285,25 @@ mapred_valgen(_Id, MaxRand) ->
     fun() ->
             list_to_binary(integer_to_list(random:uniform(MaxRand)))
     end.
+
+mapred_values( KeyGen, State ) ->
+    Keys = generate_key_list( KeyGen ),
+
+    Result = riakc_pb_socket:mapred(
+        State#state.pid,
+        Keys,
+        [{map, {modfun, riak_kv_mapreduce, map_object_value}, none, true}]
+    ),
+    % error_logger:info_msg("Result ~p~n", [Result]),
+    Result.
+
+generate_key_list( KeyGen ) ->
+    generate_key_list( 0, KeyGen, [] ).
+
+generate_key_list( 12, _KeyGen, State ) ->
+    State;
+
+generate_key_list( Counter, KeyGen, State ) ->
+    NewState = [{<<"test">>, KeyGen()}|State],
+    generate_key_list( Counter + 1, KeyGen, NewState ).
+
